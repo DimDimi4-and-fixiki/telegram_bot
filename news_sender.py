ed0e9871c2d1e9fc058767184e07bb1d98651b8a
DimDimi4-and-fixiki <kalinindima123@yandex.ru>
2020-10-29 19:09:34 +0300

    LUL

--- news_sender.py
+++ news_sender.py
@@ -118,9 +118,9 @@ class NewsSender(object):
 
                 girls_message = "Photos of girls with a bob cut  " + u'\U0001F469'
                 girls_message_rus = "Фоточки девочек с каре  " + u'\U0001F469'
-                self.bot.send_message(chat_id, girls_message_rus)
-                for num in range(user_num_of_articles):
-                    self.bot.send_photo(photo=links[num])
+                #self.bot.send_message(chat_id, girls_message_rus)
+                #for num in range(user_num_of_articles):
+                  #  self.bot.send_photo(photo=links[num])
 
 
 
